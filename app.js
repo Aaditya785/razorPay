40bbc8cc2a8597d24c2d918027d69fb77fa72caf
Aaditya785 <aadityasandil5@gmail.com>
2023-06-11 19:39:05 +0530

    update.json

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from "cors";
+import { config } from 'dotenv';
+import payment_routes from './routers/payment_routes.js';
+
+
+config({ path: "./config/config.env" });
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use("/api", payment_routes);
+app.get("/api/getkey", (req,res)=>res.status(200).json({key: process.env.RAZORPAY_API_KEY}))
+
+export default app;
