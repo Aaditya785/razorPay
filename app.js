aabd610a06b2876f634e46398e0787437dd805ea
Aaditya785 <aadityasandil5@gmail.com>
2023-06-12 10:41:27 +0530

    bug

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get("/",(req, res)=>res.send('<h1>some html</h1>'));
 app.use("/api", payment_routes);
 app.get("/api/getkey", (req,res)=>res.status(200).json({key: process.env.RAZORPAY_API_KEY}))
 
